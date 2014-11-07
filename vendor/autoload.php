86f86a4c9e8d3ae623897ba1f7718e3531eec810
Lee Smith <leesmith@openeyesolutions.co.uk>
2014-11-07 15:13:25 +0000

    Add first version

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit9b074c6d8c3508b9962047a301bffce0::getLoader();
+return ComposerAutoloaderInitca9e08eb2db12c87d9ecd1ba11a5ea45::getLoader();
